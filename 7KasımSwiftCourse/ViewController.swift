3063e6251a2ffb7822de7de9efdc9d396deb06fe
ÖZLEM ATABAY <ozlematabay@OZLEM-MacBook-Pro.local>
2015-11-07 15:52:28 +0300

    Initial Commit

---
+++ 7KasımSwiftCourse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  7KasımSwiftCourse
+//
+//  Created by ÖZLEM ATABAY on 7.11.2015.
+//  Copyright (c) 2015 ÖZLEM ATABAY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
