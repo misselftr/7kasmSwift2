9049f0fc1d8445a3ccf3c4e1caed2fde2c7f6840
ÖZLEM ATABAY <ozlematabay@OZLEM-MacBook-Pro.local>
2015-11-07 18:00:16 +0300

    dairenin çevresini hesaplayan ios uygulaması geliştirildi

--- 7KasımSwiftCourse/ViewController.swift
+++ 7KasımSwiftCourse/ViewController.swift
@@ -10,8 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func sliderChanged(sender: AnyObject) {
+        displayLabel.text=("\(radiusSelected.value)")
+    }
+    @IBOutlet weak var diaCalculate: UILabel!
+    @IBAction func resetSlider(sender: AnyObject) {
+        radiusSelected.value=0.0
+        diaCalculate.text=""
+        displayLabel.text=""
+    }
+    @IBOutlet weak var radiusSelected: UISlider!
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBAction func clickStartButton(sender: AnyObject) {
+        
+        diaCalculate.text = ("Seçilen değere göre hesaplanan çevre değeri \((radiusSelected.value)*6.28)")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0.09, green: 0.9, blue: 0.9, alpha: 1.0)
+        displayLabel.text="0.0"
+        radiusSelected.value=0.0
         // Do any additional setup after loading the view, typically from a nib.
     }
 
